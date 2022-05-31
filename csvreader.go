988834ea50da2537827f131f8c581a443714a551
Kat Jao <kat@koho.ca>
2022-05-31 18:38:25 -0400

    Learning Struct

--- csvreader.go
+++ csvreader.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/davecgh/go-spew/spew"
 )
 
@@ -10,13 +11,13 @@ func main() {
 
 	//Slices
 	// I've created an array that has 5 spaces to be filled
-	
-	/*
-	0 1 2 3 4
-	{[obj][][][obj][]}
 
-	obj
-	obj
+	/*
+		0 1 2 3 4
+		{[obj][][][obj][]}
+
+		obj
+		obj
 
 	*/
 
@@ -24,8 +25,8 @@ func main() {
 	// hey get me the object in position 3
 
 	var veggies = []string{
-		"brocolli", 
-		"mushroom", 
+		"brocolli",
+		"mushroom",
 		"potato",
 	}
 
@@ -35,11 +36,10 @@ func main() {
 
 	/*
 
-	0 1 2
-	{[brocolli][mushroom][potato]}
+		0 1 2
+		{[brocolli][mushroom][potato]}
 
 	*/
-
 
 	spices := []string{
 		"pepper",
@@ -51,10 +51,10 @@ func main() {
 	spew.Dump(spices)
 
 	/*
-	for SOME STARTING NUMBER, SOME ENDING NUMBER, increment 
+		for SOME STARTING NUMBER, SOME ENDING NUMBER, increment
 
-		do something
-	}
+			do something
+		}
 	*/
 
 	fmt.Println("---------------------------------------")
@@ -63,12 +63,14 @@ func main() {
 		fmt.Println(veggies[i])
 	}
 
-
 	//range has two vars the index number and the value at that index
-	for _, kat := range spices {
+	for i, kat := range spices {
 
 		fmt.Println(kat)
-		if kat  == "pepper" {
+		if kat == spices[i] {
+			fmt.Println("index value and range value are the same")
+		}
+		if kat == "pepper" {
 			fmt.Println("I found it!")
 		}
 
@@ -78,10 +80,46 @@ func main() {
 
 	}
 
+	type Car struct {
+		model  string
+		id     int
+		colour string
+		make   string
+	}
+
+	corolla := Car{
+		model:  "Corolla",
+		id:     1,
+		colour: "black",
+		make:   "toyota",
+	}
+
+	prius := Car{
+		model:  "Prius",
+		id:     3,
+		colour: "blue",
+		make:   "toyota",
+	}
+
+	cars := []Car{
+		corolla,
+		{
+			model:  "Rav4",
+			id:     2,
+			colour: "white",
+			make:   "toyota",
+		},
+		prius,
+	}
+
+	spew.Dump(cars)
+
+	corolla.colour = "yellow"
+	cars[0] = corolla
+	fmt.Printf("%+v", corolla)
+
+	spew.Dump(cars)
+
+	// TRANSFORM A CSV INTO A SLICE OF STRUCTS
 
 }
-
-
-
-
-
